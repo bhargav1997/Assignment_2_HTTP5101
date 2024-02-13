d513441cf577225bf0742fc7beeed6050c007824
Bhargav Suthar <sutharbhargav1997@gmail.com>
2024-02-13 18:10:33 -0500

    Update DiceGameController.cs

--- Assignment2/Controllers/DiceGameController.cs
+++ Assignment2/Controllers/DiceGameController.cs
@@ -10,6 +10,12 @@ namespace Assignment2.Controllers
     public class DiceGameController : Controller
     {
 
+        /// <summary>
+        /// Counts the number of ways to roll a sum of 10 with two dice, where the first die has m sides and the second die has n sides.
+        /// </summary>
+        /// <param name="m">The number of sides on the first die.</param>
+        /// <param name="n">The number of sides on the second die.</param>
+        /// <returns>A string representing the number of ways to roll a sum of 10.</returns>
         // GET: /api/J2/DiceGame/{m}/{n}
         [HttpGet("/api/J2/DiceGame/{m}/{n}")]
         public string CountWaysToRollTen(int m, int n)
