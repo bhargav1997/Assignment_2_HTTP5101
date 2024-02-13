384edc762e59547899b790d89e36cb39f6066a7e
Bhargav Suthar <sutharbhargav1997@gmail.com>
2024-02-13 16:05:15 -0500

    first commit

---
+++ Assignment2/Controllers/DiceGameController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2.Controllers
+{
+    [Route("api/[controller]")]
+    public class DiceGameController : Controller
+    {
+
+        // GET: /api/J2/DiceGame/{m}/{n}
+        [HttpGet("/api/J2/DiceGame/{m}/{n}")]
+        public string CountWaysToRollTen(int m, int n)
+        {
+            // Initialize count of ways to roll 10
+            int count = 0;
+
+            // Iterate through all possible values on the first die
+            for (int i = 1; i <= m; i++)
+            {
+                // Check if rolling i on first die and 10 - i on second die results in sum 10
+                if (10 - i >= 1 && 10 - i <= n)
+                {
+                    // Increment count if valid combination found
+                    count++;
+                }
+            }
+
+            // Return count of ways to roll 10 as JSON response
+            return $"Number of ways to roll the value of 10: {count}";
+        }
+    }
+}
+
---
+++ Assignment2/Controllers/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2.Controllers
+{
+    [Route("api/[controller]")]
+    public class Menu : Controller
+    {
+
+        // GET: /api/J1/Menu/{burger}/{drink}/{side}/{dessert}
+        [HttpGet]
+        [Route("/api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        public string CalculateTotalCalories(int burger, int drink, int side, int dessert)
+        {
+            // set calorie values for each menu item
+            int[] burgerCalories = { 461, 431, 420, 0 };
+            int[] drinkCalories = { 130, 160, 118, 0 };
+            int[] sideCalories = { 100, 57, 70, 0 };
+            int[] dessertCalories = { 167, 266, 75, 0 };
+
+            // Calculate total calories based on user's choice
+            int totalCalories = burgerCalories[burger - 1] + drinkCalories[drink - 1] + sideCalories[side - 1] + dessertCalories[dessert - 1];
+
+            // Return total calories as JSON response
+            return $"Your total calorie count is {totalCalories}";
+        }
+    }
+}
+
